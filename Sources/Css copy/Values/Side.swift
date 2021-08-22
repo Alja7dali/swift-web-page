068107402758a4052e29695adfb1d85a8f263a13
Abdullah Aljahdali <Alja7dali@gmail.com>
2021-08-22 22:29:49 +0300

    removing unnecessary files

--- Sources/Css copy/Values/Side.swift
+++
@@ -1,9 +0,0 @@
-public enum CSSSide: CSSValue {}
-
-public extension Value where Kind == CSSSide {
-  static let top: Self = "top"
-  static let right: Self = "right"
-  static let bottom: Self = "bottom"
-  static let left: Self = "left"
-  static let center: Self = "center"
-}
--- Sources/Css copy/Values/Unset.swift
+++
@@ -1,7 +0,0 @@
-public protocol HasValueOfUnset: CSSValue {}
-
-extension Key.ClipPath: HasValueOfUnset {}
-
-public extension Value where Kind: HasValueOfUnset {
-  static var unset: Self { return "unset" }
-}
